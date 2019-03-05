2ea4abf33751552ca26b005132d20d07437ce2b5
Alan Perez <alanp9six@gmail.com>
2019-03-05 16:59:49 -0600

    3/10 event listeners.......

--- js/index.js
+++ js/index.js
@@ -1,5 +1,38 @@
 // Your code goes here
-const bg = document.body.style.backgroundColor = "red"
+// const bg = document.body.style.backgroundColor = "red";
 
-const nav = document.querySelector("a");
+const nav = document.querySelectorAll(".nav a");
+nav.forEach(function(element){ 
+    element.addEventListener('mouseover', function() {  
+        element.style.color = 'dodgerblue';
+        element.style.opacity = '0.5'
+     });
+  })
+  nav.forEach(function(element){ 
+element.addEventListener('mouseout', function() {  
+    element.style.color = 'crimson';
+    element.style.opacity = '1'
+    });
+})
+
+//button
+const buttn = document.querySelectorAll('.btn');
+buttn[0].addEventListener('click',function(event){
+    console.log("BOOTON WAS CLICKED ZOMG 1");
+});
+buttn[1].addEventListener('click',function(event){
+    console.log("BOOTON WAS CLICKED ZOMG 2");
+});
+
+//SCROLL 
+const scroll = document.querySelector(".home");
+
+window.addEventListener("scroll", function(e){
+    console.log("scrolling....");
+    
+})
+
+//DBL CLICK
+
+
 
